71acf00b3b5d1e61c6d891ab95be452c7ab20933
emilypnascimento <emilypdonascimento@gmail.com>
2026-05-17 23:50:47 -0300

    Implementing delta time

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
 
     while True:
         log_state()
@@ -16,6 +19,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
